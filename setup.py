e3a6e714b9f786328f29e9b18f4247da4d5c6a45
GrenManSK <melnicek.richard@gmail.com>
2023-03-30 18:20:43 +0200

    Patch notes 1.0.4
    - Ability to print time statistics
    - Decision to output file with time statistics
    - Ability to return time, response or either

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import find_packages
 
 setup(
     name='final',
-    version='1.0.3',
+    version='1.0.4',
     description='final',
     author='GrenManSK',
-    install_requires=['glob2', 'tk', 'mal-api', 'urllib3', 'Pillow', 'PyAutoGUI', 'show-in-file-manager', 'tqdm', 'python-vlc', 'pygame', 'bs4',
+    install_requires=['glob2', 'tk', 'mal-api', 'urllib3', 'Pillow', 'PyAutoGUI', 'show-in-file-manager', 'tqdm', 'python-vlc', 'pygame', 'bs4', 'snakeviz',
                       'pytube', 'moviepy', 'filesplit', 'CProfileV', 'PyGetWindow', 'mysql-connector-python', 'thread6', 'bing-image-urls', 'setuptools'],
     packages=find_packages(exclude=('tests*', 'testing*')),
 )
